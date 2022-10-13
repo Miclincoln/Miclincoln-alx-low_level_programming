c3ec78174dafa05573d3c811d129f3d4c0fb2187
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2022-10-13 11:59:32 +0100

    Add comments in correct format

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*main - entry point
-  Always return 0
-*/
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
