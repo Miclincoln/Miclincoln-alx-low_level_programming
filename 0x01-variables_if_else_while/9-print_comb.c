7fb22311d5c05cc76a932f7f69b3bb65ad04b761
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2022-10-15 05:37:51 +0100

    Print all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i > 0)
+		{
+			putchar(',');
+		}
+
+		putchar(i + '0');
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
