41dff0b2129a71e4219e5a2c91ed94ddfc72fe0d
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2022-10-19 08:11:14 +0100

    Fixed bug

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,16 +29,16 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = n; i > 99; i--)
+		for (i = n; i >= 98; i--)
 		{
-			if (n > 98)
+			if (i == 98 )
 			{
 				printf("%d", i);
-				printf(", ");
 			}
 			else
 			{
 				printf("%d", i);
+				printf(", ");
 			}
 		}
 	}
