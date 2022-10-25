94456419f68862fce32ce9f283e6af4b011295c1
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2022-10-25 21:51:54 +0100

    Solutions

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -15,7 +15,7 @@ int _strlen(char *s)
 
 	while (s[i] != '\0')
 	{
-		++lenght;
+		lenght++;
 		i++;
 	}
 
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* print_rev - A function to print a string in reverse
+* @s: string
+* @i - variable
+* @lenght - variable
+* Return: void
+**/
+
+/* A function to determine the lenght of a string*/
+
+int str_lenght(char *s);
+
+void print_rev(char *s)
+{
+	int i;
+	int lenght;
+
+	lenght = str_lenght(s);
+	for (i = lenght - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+
+	}
+	_putchar('\n');
+}
+
+/**
+* str_lenght - A function to print the lenght of a string
+* @s: string
+* @a - variable
+* @lenght -variable
+* Return: lenght.
+**/
+
+int str_lenght(char *s)
+{
+	int a;
+	int lenght;
+
+	a = 0;
+	lenght = 0;
+	while (s[a] != '\0')
+	{
+		++lenght;
+		a++;
+	}
+	return (lenght);
+}
