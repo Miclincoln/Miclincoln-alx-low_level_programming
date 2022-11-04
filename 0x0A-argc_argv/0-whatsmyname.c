71bab4d956a9253bb261f11b0eec06e279c13325
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2022-11-04 16:02:30 +0100

    README and task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - a fucntion to receive argument
+* @argc: Argument count
+* @argv: Argument vector
+* Return: Always 0.
+**/
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
