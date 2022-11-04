07c4779e0b9ffe371103488aa83c9de9924e3319
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2022-11-04 09:37:47 +0100

    Fixed for betty coding style

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
 * _pow_recursion - A fucntion to print the power of a number
 * @x: Int passed to function as argument
 * @y: Int passed to function as argument
-* @pow: Return value
+* @pow - Return value
 * Return: (pow) x to the power of y
 **/
 
@@ -22,7 +22,7 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		pow = x * _pow_recursion(x, y-1);
+		pow = x * _pow_recursion(x, y - 1);
 	}
 	return (pow);
 }
