81d23868dd7b37a7380ba3aeaca022a93e266bad
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2022-11-15 15:07:00 +0100

    Readme ad tak one

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - Defined structure
+* @name: member of the struct dog
+* @age: member of the struct dog
+* @owner: member of the struct dog
+**/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
